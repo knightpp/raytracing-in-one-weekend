1818b2b2d48c06788d572966aa5b85ca1a8bc236
Danil Kondratiev <knightpp@protonmail.com>
2020-07-13 21:07:02 +0300

    fix Critical bug causing glass glitches

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,4 @@
 use super::*;
-use std::sync::Arc;
 
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
@@ -11,14 +10,12 @@ impl<T: Hittable> Hittable for Vec<T> {
         let mut hit_anything = false;
         let mut closest_so_far = t_max;
         for obj in self.iter() {
-            if obj.hit(ray, t_min, t_max, &mut hr) {
+            if obj.hit(ray, t_min, closest_so_far, &mut hr) {
                 hit_anything = true;
                 closest_so_far = hr.t;
-                //*rec = hr.clone();
-                *rec = hr.clone();
             }
         }
-
+        *rec = hr;
         hit_anything
     }
 }
